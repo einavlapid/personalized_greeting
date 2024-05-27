9a41fdb43737c02651f71f65cf555edae697c25a
einavlapid <lapideinav@gmail.com>
2024-05-27 15:28:42 +0300

    add main
    add main

---
+++ main.py
@@ -0,0 +1,11 @@
+# streamlit
+
+import streamlit as st
+
+st.title("Personalized Greeting App")
+
+name = st.text_input("Enter your name:")
+color = st.selectbox("Choose your favorite color:", ["Red", "Blue", "Green", "yellow", "pink"])
+
+if st.button("Show Greeting"):
+    st.markdown(f"<h1 style='color: {color};'>Hello, {name}!</h1>", unsafe_allow_html=True)
